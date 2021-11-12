4734171252d40171391592eac631b13cfbb3717a
Darth-Ness <75047141+Darth-Ness@users.noreply.github.com>
2021-11-12 11:48:51 +0000

    Update arch ver to debain ver level

--- arch.py
+++ arch.py
@@ -5,10 +5,16 @@ def install():
     os.system('sudo pacman -S' + " " + packageName.get())
 
 def uninstall():
-    os.system('sudo pacman -R' + " " + packageName.get())
+    os.system('sudo pacman -Rs' + " " + packageName.get())
 
 def update():
     os.system('sudo pacman -Syu')
+
+def info():
+    os.system('sudo pacman -Qi' + " " + packageName.get())
+
+def search():
+    os.system('sudo pacman -Ss' + " " + packageName.get())
 
 def installAUR():
     os.system('git clone' + packageName.get)
@@ -25,8 +31,14 @@ button2.pack(anchor=tkinter.NW, side=tkinter.LEFT)
 button3 = tkinter.Button(text="Update", command=update)
 button3.pack(anchor=tkinter.NW, side=tkinter.LEFT)
 
-button4 = tkinter.Button(text="install from AUR", command=installAUR)
+button4 = tkinter.Button(text="Info", command=info)
 button4.pack(anchor=tkinter.NW, side=tkinter.LEFT)
+
+button5 = tkinter.Button(text="Search", command=search)
+button5.pack(anchor=tkinter.NW, side=tkinter.LEFT)
+
+button7 = tkinter.Button(text="install from AUR", command=installAUR)
+button7.pack(anchor=tkinter.NW, side=tkinter.LEFT)
 
 packageName = tkinter.Entry(root, text='Hello there!')
 packageName.pack()
